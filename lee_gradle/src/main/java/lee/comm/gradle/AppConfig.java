9987734495c60235f5e008b195e1c62ee2e528bb
Lee Jung Hun <mandu965@naver.com>
2017-10-18 14:03:20 +0900

    create gradle project

---
+++ lee_gradle/src/main/java/lee/comm/gradle/AppConfig.java
@@ -0,0 +1,38 @@
+package lee.comm.gradle;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import lee.main.controller.IndexController;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter {
+ 
+    @Bean
+    public IndexController homeController(){
+        return new IndexController();
+    }
+     
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // TODO Auto-generated method stub
+        registry.addResourceHandler("/resources/**").addResourceLocations(
+                "/resources/");
+    }
+ 
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+ 
+}
+
