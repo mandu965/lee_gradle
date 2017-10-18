a459ab1ce27a2e61ff513c1d87a8f5b6605f0ac5
Lee Jung Hun <mandu965@naver.com>
2017-10-18 18:26:54 +0900

    mod

---
+++ lee_gradle/src/main/java/lee/main/controller/Index.java
@@ -0,0 +1,13 @@
+package lee.main.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Index {
+	public static void main(String[] args) {
+		System.out.println("#############################################");
+        SpringApplication.run(Index.class);
+    }
+
+}
