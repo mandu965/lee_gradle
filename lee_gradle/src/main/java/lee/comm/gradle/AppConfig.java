cb88849303078551c7394410076b7e440e866908
Lee Jung Hun <mandu965@naver.com>
2017-10-23 10:50:10 +0900

    end

--- lee_gradle/src/main/java/lee/comm/gradle/AppConfig.java
+++ lee_gradle/src/main/java/lee/comm/gradle/AppConfig.java
@@ -13,8 +13,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import lee.main.controller.MainController;
-
 @Configuration
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter {
